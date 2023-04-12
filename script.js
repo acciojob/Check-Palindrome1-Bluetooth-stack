d799e444c99d03c6b9cf6ebce07c491f0546e66f
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-12 13:10:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-
+	str = str.toLowerCase();
+	let i = 0;
+	let j = str.length;
+	while(i<=j){
+		if(str[i]!==str[j]){
+			return false;
+		}
+		else{
+			i++;
+			j--;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
